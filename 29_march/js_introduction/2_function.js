559a3d96a356850cdcd25331c6f0a7694f9e6a62
Prashant Kumar <pacific1518@gmail.com>
2022-03-31 11:40:43 +0530

    commit

---
+++ 29_march/js_introduction/2_function.js
@@ -0,0 +1,57 @@
+// // normal function
+// // a=2
+// // b=21
+// // console.log(a+b)
+// function sayhi(){
+//     console.log('hi')///function body
+
+// }
+//  sayhi()///function call or invocation
+
+//  parameter in function
+// function add(a,b){ 
+//        console.log('the addition is' ,a+b)
+// }
+// //  argument in the function
+// add(2 , 3)
+
+// function mul(a,b){
+//         console.log('the mul is',a*b)
+// }
+// mul(2*3)
+
+// function are treated asfirst class citizen in javascript
+// let sayhi = function() {
+//     console.log('hi')
+// }
+
+// sayhi()
+// function add(a,b){
+//   console.log('the addition is',a+b)
+// }
+//   add(1+3)
+
+
+let multiply=function(a,b){
+    console.log(a*b)
+}
+multiply(2*5)
+
+
+
+
+
+// IIFE  (immediately invoked function expression)
+ 
+let substract=(function(a,b){
+    console.log(a-b)
+})   (30,10)
+
+
+
+
+
+
+
+
+
